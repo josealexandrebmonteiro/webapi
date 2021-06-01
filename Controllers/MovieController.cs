c66659beb926b1efab217de22d74862d27ff9a3f
Jose Alexandre Monteiro <josealexandrebmonteiro@gmail.com>
2021-05-31 21:57:21 -0300

    swagger para abrir direto

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    public MovieController (){
+
+    }
+
+    [HttpPost]
+    public string Post(){
+        return "Post";
+    }
+
+    [HttpPut]
+     public string Put(){
+        return "Put";
+    }
+
+    [HttpGet]
+    public string Get(){
+        return "Get";
+    }
+
+    [HttpDelete]
+    public string Delete(){
+        return "Delete";
+    }
+
+    
+    
+
+}
--- Startup.cs
+++ Startup.cs
@@ -42,6 +42,7 @@ namespace webapi
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webapi v1"));
+                c.RoutePrefix = string.Empty; //onde mudei
             }
 
             app.UseHttpsRedirection();
